455f5c62e63f58862bbb08b4a5ceeed0937e9c91
MMudasir4135 <108607214+MMudasir4135@users.noreply.github.com>
2022-07-06 14:02:38 +0500

    Add files via upload

---
+++ PROJECT/SalarySlip.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salarySlip
+{
+
+    public class SalarySlip
+    {
+        private string Designation;
+        private int grossSalary;
+
+        public string getDesignation()
+        {
+            return Designation;
+        }
+
+        public void setDesignation()
+        {
+            Console.WriteLine("Enter your designation:");
+            Designation = Console.ReadLine();
+        }
+
+
+        public void Salary()
+        {
+            if (Designation == "engineer")
+            {
+                Employee E = new Employee();
+                grossSalary = E.baseSalary + E.Allowance();
+                Console.WriteLine("The gross salary is:" + grossSalary);
+
+            }
+            else if (Designation == "manager")
+            {
+                Manager M = new Manager();
+                grossSalary = M.baseSalary + M.Allowance();
+                Console.WriteLine("The gross salary is:" + grossSalary);
+
+            }
+            else if (Designation == "analyst")
+            {
+                Analyst A = new Analyst();
+                grossSalary = A.baseSalary + A.Allowance();
+                Console.WriteLine("The gross salary is:" + grossSalary);
+
+            }
+
+            else
+            {
+                Console.WriteLine("The designation entered is incorrect");
+            }
+
+
+        }
+    }
+
+    class Employee
+    {
+        public int baseSalary = 1500;
+        public int Fuel;
+        public int Medical;
+        public virtual int Allowance()
+        {
+            Fuel = 100;
+            Medical = 500;
+
+            return (Fuel + Medical);
+        }
+
+    }
+
+
+    class Manager : Employee
+    {
+        public override int Allowance()
+        {
+            Fuel = 250;
+            Medical = 1000;
+
+            return (Fuel + Medical);
+        }
+
+
+    }
+
+
+    class Analyst : Employee
+    {
+        public override int Allowance()
+        {
+             Fuel = 150;
+            Medical = 800;
+
+            return (Fuel + Medical);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalarySlip S = new SalarySlip();
+
+            S.setDesignation();
+        
+            S.Salary();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Project1/SalarySlip/SalarySlip/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+
+namespace SalarySlip
+{
+
+    public class SalarySlip
+    {
+        string Designation;
+
+
+        public string getDesignation()
+        {
+            return Designation;
+        }
+
+        public void setDesignation()
+        {
+            Console.WriteLine("Enter employee designation:");
+            Designation = Console.ReadLine();
+        }
+
+
+        public void Salary()
+        {
+            if (Designation == "engineer")
+            {
+                Engineer E = new Engineer();
+                int Salary = E.baseSalary + E.Allowance();
+                Console.WriteLine("The net salary is:" + Salary);
+
+            }
+            else if (Designation == "manager")
+            {
+                Manager M = new Manager();
+                int Salary = M.baseSalary + M.Allowance();
+                Console.WriteLine("The net salary is:" + Salary);
+
+            }
+            else if (Designation == "analyst")
+            {
+                Analyst A = new Analyst();
+                int Salary = A.baseSalary + A.Allowance();
+                Console.WriteLine("The net salary is:" + Salary);
+
+            }
+
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+
+
+        }
+    }
+
+    class Engineer : SalarySlip
+    {
+        public int baseSalary = 1500;
+        public int Fuel;
+        public int Medical;
+        public virtual int Allowance()
+        {
+            Fuel = 100;
+            Medical = 500;
+
+            return (Fuel + Medical);
+        }
+
+    }
+
+
+    class Manager : Engineer
+    {
+        public override int Allowance()
+        {
+            Fuel = 250;
+            Medical = 1000;
+
+            return (Fuel + Medical);
+        }
+
+
+    }
+
+
+    class Analyst : Engineer
+    {
+        public override int Allowance()
+        {
+            Fuel = 150;
+            Medical = 800;
+
+            return (Fuel + Medical);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalarySlip S = new SalarySlip();
+
+            S.setDesignation();
+
+            S.Salary();
+            Console.ReadLine();
+        }
+    }
+}
